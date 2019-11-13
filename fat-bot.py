861fe72210793c9bd7d7cc0e2782d8798fef9bab
Tim Schmidt <tim.schmidt@ewe.net>
2019-11-13 15:44:11 +0100

    some commenting structure

--- fat-bot.py
+++ fat-bot.py
@@ -14,15 +14,25 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+################################################################################
+## Bot commands
+################################################################################
+
+##############################
+# Author: Tim | w4rum
+# DateCreated: 11/13/2019
+# Purpose: DM all members of the specified roles with the specified message
+###############################
 @bot.command()
 async def dm(ctx, *args):
+    # Check arugment structure
     if len(args) < 2:
-        await ctx.send("Usage: `~dm ROLE [MORE ROLES...] MESSAGE`")
+        await send_usage_help(ctx, "dm", "ROLE [MORE ROLES...] MESSAGE")
 
     message = args[-1]
     roles = args[:-1]
 
-    # Filter roles with invalid format
+    # Check for roles with invalid format
     # The Discord client translates role mentions into "<@&ID>",
     # with ID being numerical
     invalid_roles = \
@@ -34,7 +44,29 @@ async def dm(ctx, *args):
                 + ", ".join(invalid_roles))
         return
 
+
+################################################################################
+## Utility functions
+################################################################################
+
+##############################
+# Author: Tim | w4rum
+# DateCreated: 11/13/2019
+# Purpose: Send an error message to the current chat
+###############################
 def send_error(ctx, text):
     return ctx.send("[ERROR] " + text)
 
-bot.run(BOT_TOKEN)
+
+##############################
+# Author: Tim | w4rum
+# DateCreated: 11/13/2019
+# Purpose: Send a usage help to the current chat
+###############################
+def send_usage_help(ctx, function_name, argument_structure):
+    return ctx.send("Usage: `%s%s %s`" \
+                    % (BOT_CMD_PREFIX, function_name, argument_structure))
+
+
+if __name__ == "__main__":
+    bot.run(BOT_TOKEN)
