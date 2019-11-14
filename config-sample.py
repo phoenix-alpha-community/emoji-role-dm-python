5fc22556613eddf86344ae8a3cf7bd7d3916f025
Tim Schmidt <tim.schmidt@ewe.net>
2019-11-14 21:58:12 +0100

    fixed categories in config-sample.py

--- config-sample.py
+++ config-sample.py
@@ -1,9 +1,9 @@
 # Required settings (must be changed)
 BOT_TOKEN               = "INSERT BOT TOKEN HERE"
-BOT_DM_CHANNEL          = "bot-comma"
-BOT_ROLE_CHANNEL        = ["game-roles", "side-game-channels", "hobbies-channels"]
 
 # Optional settings (may be changed)
 BOT_DESCRIPTION         = "Fear and Terror Discord Bot"
 BOT_CMD_PREFIX          = "~"
 BOT_DM_REQUIRED_ROLE    = "GEN"
+BOT_ROLE_CHANNEL        = ["game-roles", "side-game-channels", "hobbies-channels"]
+BOT_DM_CHANNEL          = "bot-comma"
