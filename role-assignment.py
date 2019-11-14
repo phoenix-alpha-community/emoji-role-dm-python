588a28ff47df11d501dd96142a018145344db632
LolloAntoni28 <lorant04@outlook.it>
2019-11-14 18:31:16 +0100

    Added new role assignment function

--- role-assignment.py
+++ role-assignment.py
@@ -1,6 +1,7 @@
 # IMPORT
 from config import *
 
+
 # FUNCTION
 def role_assignment():
 	# CLASS
@@ -23,11 +24,13 @@ def role_assignment():
 
 	# CODE
 	@client.event
-	async def on_reaction_add(reaction, user):
+	async def on_raw_reaction_add(payload):
+		print("done")
 		if reaction.message.channel == side_game.channel:
 			if reaction.message.id == side_game.message:
 				i = 0
 				for emoji in side_game.reactions:
+					print(emoji, reaction)
 					if emoji == reaction:
 						member = await reaction.message.guild.fetch_member(user.id)
 						if not member:
@@ -40,3 +43,13 @@ def role_assignment():
 		elif reaction.message.channel == hobbies.channel:
 			if reaction.message.id == hobbies.message:
 				pass
+
+	@client.event
+	async def on_message(message):
+		print(message.content)
+
+	client.run("NjQ0MTI4NDk2NDY5MTQ3NjU4.XcvhlA.eQ9qdgNWTJvsfykObMXIAr3mPy0")
+
+
+if __name__ == "__main__":
+	role_assignment()
